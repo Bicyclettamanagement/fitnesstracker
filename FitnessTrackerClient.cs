0e0a7b4aa20eba5f8792de80e2b0ddd3e537d919
Jonas Köblin <kojo8@hotmail.de>
2023-03-27 14:59:55 +0200

    StartMenu and Exit added

--- FitnessTrackerClient.cs
+++ FitnessTrackerClient.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,28 +17,81 @@ namespace FitnessTracker
         {
             this.ui = ui;
             this.db = db;
-            
         }
-        public Athlete Login(string username)
+        public void Start()
         {
-            ui.Write("Please enter your Password:");
-            while (true)
+            ui.Write("Welcome to Fitnesstracker!");
+            ui.Write("Please enter your Username:");
+            this.User = this.LoginSuccessful(ui.Read());
+            this.StartMenu();
+        }
+        public void Stop()
+        {
+            ui.Write("Exiting...");
+            Environment.Exit(0);
+        }
+        public Athlete LoginSuccessful(string? username)
+        {
+            if (db.UsernameExists(username))
             {
-                string? password = ui.Read();
-                if (password != null)
+                ui.Write("Please enter your Password:");
+                while (true)
                 {
-                    if (db.PasswordCorrect(username, password)) { return db.GetAthleteByUsername(username); }
-                    else { ui.Write("Wrong password"); }
+                    string? password = ui.Read();
+                    if (password != null)
+                    {
+                        if (db.PasswordCorrect(username, password))
+                        {
+                            return db.GetAthleteByUsername(username);
 
+                        }
+                        else
+                        {
+                            ui.Write("Wrong password");
+
+                        }
+                    }
                 }
+            } else
+            {
+                return this.CreateAccount(username);
             }
-            
-            
         }
         public Athlete CreateAccount(string username)
         {
             return db.GetAthleteByUsername(username);
         }
-
+        public void StartMenu()
+        {
+            while(true)
+            {
+                ui.Write("1: Start new Workout");
+                ui.Write("2: Recent Workouts");
+                ui.Write("X: Exit");
+                switch (ui.Read())
+                {
+                    case "1":
+                        StartWorkout();
+                        break;
+                    case "2":
+                        ViewRecentWorkouts();
+                        break;
+                    case "x":
+                    case "X":
+                        this.Stop();
+                        break;
+                    default:
+                        break;
+                }
+            }            
+        }
+        public void StartWorkout()
+        {
+            ui.Write("this is a new workout");
+        }
+        public void ViewRecentWorkouts()
+        {
+            ui.Write("Recent Workouts");
+        }
     }
 }
