ab7e052cdf65cc1d354a92676979f29edcc6b675
Jonas Kölblin <kojo8gonow@gmail.com>
2023-05-31 22:50:14 +0200

    added ExerciseDecorator

---
+++ fitnesstracker-project/Domain/PerformedExerciseDecorator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessTracker.Domain
+{
+    public abstract class ExerciseDecorator : PerformedExercise
+    {
+        protected PerformedExercise _exercise;
+
+        public ExerciseDecorator(PerformedExercise exercise)
+            : base(exercise.ExerciseId, 0, exercise.Weight)
+        {
+            _exercise = exercise;
+        }
+    }
+
+    public class TimeExerciseDecorator : ExerciseDecorator
+    {
+        public int TimeInSeconds { get; set; }
+
+        public TimeExerciseDecorator(PerformedExercise exercise, int timeInSeconds)
+            : base(exercise)
+        {
+            TimeInSeconds = timeInSeconds;
+        }
+    }
+
+}
